67b840af657b195763c3302f1cf85738cff4be32
ZZZaky <yurakalinin29@gmail.com>
2024-11-13 20:13:03 +0300

    Добавил логику чекпоинтов и менеджера чекпоинтов

---
+++ Assets/Development/Scripts/Checkpoints/Checkpoint.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+// Чекпоинт
+//
+public class Checkpoint : MonoBehaviour
+{
+    public int checkpointNumber;
+
+    public CheckpointManager checkpointManager;
+    public bool checkpointPassed;
+
+    void Start()
+    {
+        checkpointPassed = false;
+    }
+
+    public void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player" && !checkpointPassed)
+        {
+            checkpointManager.OnEnterCheckpoint(checkpointNumber);
+        }
+    }
+
+    public void Delete()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Development/Scripts/Checkpoints/CheckpointManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Менеджер чекпоинтов
+//
+public class CheckpointManager : MonoBehaviour
+{
+    public List<Checkpoint> allCheckpoints;
+
+    public void OnEnterCheckpoint(int checkpoint)
+    {
+        if (checkpoint == 0 || allCheckpoints[checkpoint - 1].checkpointPassed) 
+        {
+            DoneCheckpoint(checkpoint);
+        }
+        if (checkpoint ==  allCheckpoints.Count - 1)
+        {
+            Finish();
+        }
+    }
+
+    public void DoneCheckpoint(int checkpoint)
+    {
+        allCheckpoints[checkpoint].checkpointPassed = true;
+    }
+
+    public void AddCheckpoint(Checkpoint checkpoint)
+    {
+        allCheckpoints.Add(checkpoint);
+    }
+
+    public void DeleteCheckpoint(int checkpoint)
+    {
+        allCheckpoints.RemoveAt(checkpoint);
+    }
+
+    public void ResetCheckpoints()
+    {
+        foreach (var checkpoint in allCheckpoints)
+        {
+            checkpoint.Delete();
+        }
+        allCheckpoints = new List<Checkpoint>();
+    }
+
+    private void Finish()
+    {
+        // TODO
+    }
+}
