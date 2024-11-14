0e2991bebdd8eda729f28ae6a4db66b21c3006cc
ZZZaky <yurakalinin29@gmail.com>
2024-11-14 23:19:38 +0300

    Добавил создание кривой Безье в менеджер чекпоинтов

--- Assets/Development/Scripts/Checkpoints/BezierFactory.cs
+++ Assets/Development/Scripts/Checkpoints/BezierFactory.cs
@@ -10,6 +10,23 @@ public class BezierFactory : MonoBehaviour
 
     void Start()
     {
-        Debug.Log(spline[0].position);
+        Debug.Log(spline[0].position.GetType());
+    }
+
+    public void CreatePoints(List<Vector3> points)
+    {
+        Debug.Log("Creating points...");
+        spline.Initialize(points.Count);
+        for (int i = 0; i < points.Count; i++)
+        {
+            spline[i].position = points[i];
+        }
+        spline.AutoConstructSpline();
+    }
+
+    public void AddPoint(Vector3 point)
+    {
+        spline.InsertNewPointAt(spline.Count);
+        spline[spline.Count - 1].position = point;
     }
 }
--- Assets/Development/Scripts/Checkpoints/CheckpointManager.cs
+++ Assets/Development/Scripts/Checkpoints/CheckpointManager.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class CheckpointManager : MonoBehaviour
 {
     public List<Checkpoint> allCheckpoints;
+    public BezierFactory bezierFactory;
 
     void Start()
     {
@@ -15,41 +16,17 @@ public class CheckpointManager : MonoBehaviour
         {
             allCheckpoints = new List<Checkpoint>();
         }
-
-
-
-
-        // TEST //
-
-        // y = k*x + b
-        // k - угловой коэффициент (тангенс угла, образованного данной прямой и положительным направлением оси 0Y (0Z))
-        // b - свободный коэффициент (пересечение с осью 0X)
-        // 
-
-        double global_x1 = -20, global_y1 = -20, angle1 = -45;
-        double global_x2 = 0, global_y2 = -30, angle2 = -120;
-
-
-        double x1 = global_x1 - global_x2;
-        double x2 = 0;
-
-        double y1 = 0;
-        double y2 = global_y2 - global_y1;
-
-        double b1 = 0;
-        double b2 = y2;
-
-        double k1 = System.Math.Round(System.Math.Tan(angle1), 1);
-        double k2 = System.Math.Round(System.Math.Tan(angle2), 1);
-
-        double x = -(b1 - b2) * (k1 - k2);
-        double y = k1 * x + b1;
-
-        //Debug.Log($"{y1} = {k1}*{x1} + {b1}");
-        //Debug.Log($"{y2} = {k2}*{x2} + {b2}");
-        //Debug.Log($"x: {x}, z: {y}");
-
-        // TEST //
+        else
+        {
+            Debug.Log(allCheckpoints.Count);
+            List<Vector3> points = new List<Vector3>();
+            foreach (var checkpoint in allCheckpoints) 
+            { 
+                Debug.Log(checkpoint.transform.position);
+                points.Add(checkpoint.transform.position);
+            }
+            bezierFactory.CreatePoints(points);
+        }
     }
 
     public void OnEnterCheckpoint(int checkpoint)
